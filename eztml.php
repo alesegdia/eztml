549e7933295909a7b7e7666b6e09a210630e64c1
Alejandro Seguí <alesegdia@gmail.com>
2015-07-17 20:18:41 +0200

    adding eztml script

---
+++ eztml.php
@@ -0,0 +1,50 @@
+<?php
+
+$str = file_get_contents("php://stdin");
+
+$jsonstr = file_get_contents("config.json");
+
+$jsondata = json_decode($jsonstr, true);
+
+var_dump($jsondata);
+
+$pres = $jsondata["pres"];
+$posts = $jsondata["posts"];
+
+function wrap( $text, $options ) {
+	global $pres, $posts;
+	$retval = $text;
+	$options = explode(",", $options);
+	foreach( $options as $option ) {
+		if( !array_key_exists($option, $pres) || !array_key_exists($option, $posts) ) {
+			exit("option '$option' not configured! check your config.json\n");
+		}
+		$retval = $pres[$option] . $retval . $posts[$option];
+	}
+	return $retval;
+}
+
+function parsehtml( $text ) {
+	global $str;
+	$regex = "/({%=\(([a-z]*),?\)(.*)%})/";
+	$regex = "/{%=\(((([a-z]*),?)*)\)((?:[^{}]+|(?R))*)%}/";
+	preg_match_all($regex, $text, $matches, PREG_SET_ORDER);
+	$processing = true;
+	while( $processing ) {
+		if( preg_match_all($regex, $text, $matches, PREG_SET_ORDER) > 0 ) {
+			$processing = true;
+			foreach( $matches as $match ) {
+				$match_ret = parsehtml($match[4]);
+				$replacement = wrap($match[4], $match[1]);
+				$text = str_replace($match[0], $replacement, $text);
+			}
+		} else {
+			$processing = false;
+		}
+	}
+	return $text;
+}
+
+print(parsehtml($str));
+
+?>
